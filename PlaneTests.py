6335a88398aec8745a49edc14c3841356c2cc721
Sureka Stoeckigt <surekastoeckigt@surekas-mbp.home>
2019-06-30 22:47:29 +0100

    comparing tests to ruby script from week1

--- PlaneTests.py
+++ PlaneTests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from plane import Plane
+from plane import *
 
 class TestPlane(unittest.TestCase):
 
@@ -8,26 +8,30 @@ class TestPlane(unittest.TestCase):
     def setUp(self):
         self.plane = Plane()
 
-    def test_0_defaults(self):
-        """Plane initially flying"""
-        self.assertTrue(self.plane.isFlying)
+    # def test_0_defaults(self):
+    #     """Plane initially flying"""
+    #     self.assertTrue(self.plane.isFlying)
 
-    def test_1_land_changes_isFlying_to_false(self):
-        """Plane changes status when landed"""
-        self.plane.land()
-        self.assertFalse(self.plane.isFlying)
+    # def test_1_land_changes_isFlying_to_false(self):
+    #     """Plane changes status when landed"""
+    #     self.plane.land()
+    #     self.assertFalse(self.plane.isFlying)
 
-    def test_2_takeoff_changes_isflying_to_true(self):
-        """Plane changes status twice when landed and taking off"""
-        self.plane.land()
-        self.plane.take_off()
-        self.assertTrue(self.plane.isFlying)
+    # def test_2_takeoff_changes_isflying_to_true(self):
+    #     """Plane changes status twice when landed and taking off"""
+    #     self.plane.land()
+    #     self.plane.take_off()
+    #     self.assertTrue(self.plane.isFlying)
 
-    def test_3_raise_exception_if_trying_to_takeoff_while_flying(self):
-        self.plane.land()
-        self.plane.take_off()
-        with self.assertRaisesRegexp(Exception, 'Plane cannot takeoff while flying'):
-            self.plane.take_off()
+    # def test_3_raise_exception_if_trying_to_takeoff_while_flying(self):
+    #     self.plane.land()
+    #     self.plane.take_off()
+    #     with self.assertRaisesRegexp(Exception, 'Plane cannot takeoff while flying'):
+    #         self.plane.take_off()
+        
+    def test_2_set_request(self): 
+        self.plane.setRequestToLand()
+        self.assertTrue(self.plane.requestedToLand)
 
 if __name__ == '__main__':
         unittest.main()
--- plane.py
+++ plane.py
@@ -2,20 +2,27 @@ class Plane():
 
     def __init__(self):
         self.isFlying = True
+        self.requestedToLand = False
 
 #giving Plane class plane attribute
     def setUp(self):
         self.plane = Plane()
         # self.airport = ""
 
-    def land(self):
-        self.isFlying = False
-        # self.airport = airport
-    #
-    def take_off(self):
-        if self.isFlying == True:
-            raise Exception('Plane cannot takeoff while flying')
-        else:
-            self.isFlying = True
+    def setRequestToLand(self):
+        self.plane = Plane()
+        self.requestedToLand = True
+    # def land(self):
+    #     self.isFlying = False
+    #     # self.airport = airport
+    # #
+    # def take_off(self):
+    #     if self.isFlying == True:
+    #         raise Exception('Plane cannot takeoff while flying')
+    #     else:
+    #         self.isFlying = True
     # def is_at_airport(self):
     #     return self.__at_airport
+
+
+        
